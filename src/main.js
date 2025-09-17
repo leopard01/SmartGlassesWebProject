9d2e546d39d416da8c5b35932709cbfc3aea1503
cui <30600383+csq529@users.noreply.github.com>
2025-09-17 16:23:39 +0800

    添加vconsole

--- src/main.js
+++ src/main.js
@@ -2,13 +2,25 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'lib-flexible/flexible'
+import { Button } from 'vant';
+import 'vant/lib/index.css';
 
 import App from './App.vue'
 import router from './router'
+const env = import.meta.env.VITE_APP_ENV
+import VConsole from 'vconsole';
+console.log('env:::::::', env)
+if (env !== 'production') {
+    new VConsole();
+}
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Button);
 
 app.mount('#app')
+
+
